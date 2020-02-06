57215efa4cb2195f76b1580537bdaf1fad514fec
Vahidito <vahidito@gmail.com>
2020-02-06 20:16:01 +0330

    for check

--- practice/warm up/BMI2.py
+++ practice/warm up/BMI2.py
@@ -12,7 +12,7 @@ else:
     weight = float(weight)
     height = float(height)
     bmi = weight / (height * 100) ** 2
-
+#if clause
 if bmi <= 18.5:
     print("Underweight")
 elif 18.5 < bmi <= 25:
