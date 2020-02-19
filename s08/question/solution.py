7699ff93fb5177c790fcca803deddc98a5fe59e7
iamvee <naeini.v@gmail.com>
2020-02-19 08:21:48 -0800

    pascal

--- s08/question/solution.py
+++ s08/question/solution.py
@@ -1,16 +1,21 @@
-# step 1:
-fr = open('input.txt', mode='r')
-text = fr.read()
-n, pat = text.strip().split('\n')
-n = int(n)
-fr.close()
+def read_file(file_name: str) -> tuple:
+    global n, pat
+    fr = open(file_name, mode='r')
+    text = fr.read()
+    n, pat = text.strip().split('\n')
+    n = int(n)
+    fr.close()
+    return n, pat
 
-# step 2:
 
-out_line = pat * n + '\n'
-out_text = out_line * n
+def write_file(file_name: str, pat: str, n: int):
+    fw = open(file_name, mode='w')
+    for i in range(n):
+        for j in range(n):
+            fw.write(pat)
+        fw.write('\n')
+    fw.close()
 
-# step 3:
-fw = open('output.txt', mode='w')
-fw.write(out_text)
-fw.close()
+
+# number_of_lines, pattern = read_file("input.txt")
+write_file("output.txt", pattern, number_of_lines)
