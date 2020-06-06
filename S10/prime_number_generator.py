ab588878470fd4ca310d411940840c84875ddb68
iamvee <naeini.v@gmail.com>
2020-06-06 06:39:01 -0700

    solution (using lists)

--- S10/prime_number_generator.py
+++ S10/prime_number_generator.py
@@ -1,13 +1,21 @@
-def primes_generator(n: int):
+
+def is_prime(n: int):
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+
+def primes_generator(upper_limit: int):
     lst = []
-    #
-    #
-    #
-    #
+    for i in range(2, upper_limit):
+        if is_prime(i):
+            lst.append(i)
     return lst
 
 
-primes = primes_generator(1000)
+primes = primes_generator(100)
+print(primes)
 
-for prime in primes:
-    print(prime)
+# for prime in primes:
+#     print(prime)
