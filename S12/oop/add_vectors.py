559cae43639b094dfcf1d686d241b5c5c5c5d99f
iamvee <naeini.v@gmail.com>
2020-06-13 07:11:41 -0700

    add vcetor

--- S12/oop/add_vectors.py
+++ S12/oop/add_vectors.py
@@ -11,15 +11,14 @@ class Vector:
         result = math.sqrt(temp)
         return result
 
-
-def add_vectors(a: Vector, b: Vector) -> Vector:
-    x_temp = a.x + b.x
-    y_temp = a.y + b.y
-    return Vector(x_temp, y_temp)
+    def add_to_another_vector(self, other):
+        x_temp = self.x + other.x
+        y_temp = self.y + other.y
+        return Vector(x_temp, y_temp)
 
 
 v1 = Vector(4, 3)
 v2 = Vector(12, 5)
-v3 = add_vectors(v1, v2)
+v3 = v1.add_to_another_vector(v2)
 
 print(v3.x, v3.y, v3.length())
