02507d364857dc6f125cb77716e1ad890e69f4dd
iamvee <naeini.v@gmail.com>
2020-06-27 20:04:34 +0430

    geom

--- S15/oop/geometry_4.py
+++ S15/oop/geometry_4.py
@@ -1,8 +1,6 @@
 class Area:
     def __get__(self, instance, owner):
-        if owner == Square:
-            return instance.side * instance.side
-        elif owner == Rectangle:
+        if owner == Rectangle or owner == Square:
             return instance.width * instance.height
         elif owner == Circle:
             return instance.radius * instance.radius * 3.14
@@ -10,38 +8,31 @@ class Area:
 
 class Perimeter:
     def __get__(self, instance, owner):
-        if owner == Square:
-            return instance.side * 4
-        elif owner == Rectangle:
+        if owner == Rectangle or owner == Square:
             return (instance.width + instance.height) * 2
         elif owner == Circle:
             return 2 * instance.radius * 3.14
 
 
-class Square:
+class Geometry:
     area = Area()
     perimeter = Perimeter()
 
-    def __init__(self, a):
-        self.side = a
 
-
-class Rectangle:
-    area = Area()
-    perimeter = Perimeter()
-
+class Rectangle(Geometry):
     def __init__(self, a, b):
         self.width = a
         self.height = b
 
 
-class Circle:
-    area = Area()
-    perimeter = Perimeter()
+class Square(Rectangle):
+    def __init__(self, a):
+        super().__init__(a, a)
 
+
+class Circle(Geometry):
     def __init__(self, r):
         self.radius = r
-
 
 
 sq = Square(10)
