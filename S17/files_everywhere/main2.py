b8d0af74d02774f9c1f091a4cf0111ecd5bb6545
iamvee <naeini.v@gmail.com>
2020-07-04 20:23:56 +0430

    files 3

--- S17/files_everywhere/main2.py
+++ S17/files_everywhere/main2.py
@@ -20,5 +20,60 @@ def get_child_to_parent(directory, extention):
     return child_to_parent
 
 
-result = get_child_to_parent('./files', 'txt')
-print(result)
+def get_nodes_with_no_parent(c2p):
+    lst = []
+
+    for k in c2p:
+        if c2p[k] == '0':
+            lst.append(k)
+    return lst
+
+
+def get_parent_to_children(p2c):
+    out = {}
+
+    for k, v in p2c.items():
+        if k not in out:
+            out[k] = []
+
+        if v not in out:
+            out[v] = []
+
+        out[v].append(k)
+
+    return out
+
+
+def get_nodes_with_no_children(out):
+    lst = []
+
+    for k in out:
+        if out[k] == []:
+            lst.append(k)
+    return lst
+
+
+def ancestors(c2p, node_number):
+    res = []
+    while True:
+        res.append(node_number)
+        node_number = c2p[node_number]
+        if node_number == '0':
+            break
+
+    res.append(node_number)
+    return res
+
+
+
+c2p = get_child_to_parent('./files', 'txt')
+print(c2p)
+no_parent = get_nodes_with_no_parent(c2p)
+print(no_parent)
+p2c = get_parent_to_children(c2p)
+print(p2c)
+no_child = get_nodes_with_no_children(p2c)
+print(no_child)
+
+for i in no_child:
+    print("***", ancestors(c2p, i))
